10051ea55f67dbd5271add53a3fd06a71a779c72
Sachin Jangir <support@ItzSjDude.in>
2020-11-19 05:41:15 +0530

    Update startup.sh

--- init/startup.sh
+++ init/startup.sh
@@ -3,8 +3,11 @@
 . init/Deps.sh 
 
 echo 'Cleaning up 'Pikabot'
-rm -rf ./ && rm -rf ./.gitignore && rm -rf ./.git
+rm -rf ./* && rm -rf ./.gitignore && rm -rf ./.git
 
 echo 'Getting Source Ready' 
 git clone -b Beta https://github.com/ItzSjDude/PikachuUserbot ./
 
+mkdir ./pikabot/main_plugs
+
+python -m pikabot
