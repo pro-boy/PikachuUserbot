1a8c5754404c57cc7cf51b53f87994e5ed091526
Sachin Jangir <support@ItzSjDude.in>
2020-11-19 07:46:40 +0530

    Update Deps.sh

--- init/Deps.sh
+++ init/Deps.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-insReq() {
+_cleanup() {
+    echo 'Cleaning up Pikabot'
+    rm -rf ./* && rm -rf ./.gitignore && rm -rf ./.git
+} 
+
+_source() {
+    echo 'Getting Source Ready' 
+    git clone -b Beta https://github.com/ItzSjDude/PikachuUserbot ./
+}
+
+_upgradePip() {
+    echo '••• Updating Pip •••' 
+    pip3 install -U pip &> /dev/null
+    echo 'Updated Pip 🚶'
+}
+
+_insReq() {
     echo '••• Installing Requirements •••'
     pip3 install -r $1/requirements.txt &> /dev/null
     echo 'Installed Requirements 🚶'
 }
 
-upgradePip() {
-    echo '••• Updating Pip •••' 
-    pip3 install -U pip &> /dev/null
-    echo 'Updated Pip 🚶'
+start() {
+    _cleanup
+    _source
+    _upgradePip
+    _insReq
+    mkdir ./pikabot/main_plugs
+    python3 -m pikabot
 }
