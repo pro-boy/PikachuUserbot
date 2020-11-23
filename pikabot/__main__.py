54fd421eecd36ec647f613bdd17c5f84e0862ba4
Sachin Jangir <support@ItzSjDude.in>
2020-11-23 22:28:42 +0530

    Update __main__.py

--- pikabot/__main__.py
+++ pikabot/__main__.py
@@ -1,4 +1,8 @@
 import os, telethon, telethon.utils, asyncio, traceback ; from pikabot import * ; from sys import * ; from var import * ; client = bot ; ItzSjDude = client ; from telethon.errors.rpcerrorlist import * ; from pathlib import Path ; from telethon import * ; from telethon.tl.types import *;a = Pk(pid).decode('utf-8');Client = pk+a
 
 if bot is None: 
-    import pikabot.login 
+    from pikabot.login import *
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+else:
+    pass
