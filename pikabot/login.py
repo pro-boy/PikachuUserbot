e19b09e45bc6b32a5bfd5c48d3a912cc9352735d
Sachin Jangir <support@ItzSjDude.in>
2020-11-24 09:39:10 +0530

    Update login.py

--- pikabot/login.py
+++ pikabot/login.py
@@ -8,25 +8,40 @@ from var import Var
 Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
 app = Heroku.app(Var.HEROKU_APP_NAME)
 
-_phone_ ="**Enter your Phone no. On which u want @PikachuUserbot 😛**\nIf Indian No. **+91xxxxxxxxxx** else use **Country Code**"
-_2vfa_ = "Seems like u have **2-Step verification** On your Account. Enter Your Password"
-_verif_= "Please enter the verification code that you receive from Telegram\n**if your code is** 06969 **then enter** 0 6 9 6 9."
-_code_ = "**Invalid Code Received**. Please /start"
-async def main():
+
+def get_client(_Pika_):
+    global a
+    if _Pika_=="STRING_SESSION":
+       a ="**MAINCLIENT**"
+    elif _Pika_=="STR2":
+      a ="**MULTICLIENT1**"
+    elif _Pika_=="STR3":
+      a ="**MULTICLIENT2**"
+    elif _Pika_=="STR4":
+      a ="**MULTICLIENT3**"
+    return a
+      
+_phone_ ="**Login Assistent** For {}\n\nEnter your Phone no. On which u want @PikachuUserbot 😛\nIf Indian No. **+91xxxxxxxxxx** else use **Country Code**"
+_2vfa_ = "**Login Assistent** For {}\n\nSeems like u have **2-Step verification** On your Account. Enter Your Password"
+_verif_= "**Login Assistent** For {}\n\nPlease enter the verification code that you receive from Telegram\n**if your code is** 06969 **then enter** 0 6 9 6 9."
+_code_ = "**Login Assistent** For {}\n\n**Invalid Code Received**. Please /start"
+_logged_ = "Login Assistent** For {}\n\n **LOGGED IN** Successfully wait for 2Mins n then Do .pika**"
+async def pika_login(_PiKa_):
     _PikaBot_ = await TelegramClient(
         "PikaBot",
         Var.APP_ID,
         Var.API_HASH
     ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+    _cn_=get_client_(_PiKa_)
     async with _PikaBot_:
         me = await _PikaBot_.get_me()
-        logging.info(me.stringify())
+        logging.info(me.first_name)
         @_PikaBot_.on(events.NewMessage())
         async def handler(event):
             APP_ID = Var.APP_ID;API_HASH = Var.API_HASH
             Config=app.config()  
             async with event.client.conversation(event.chat_id) as conv:
-                await conv.send_message(_phone_)
+                await conv.send_message(_phone_.format(_cn_))
                 pikaget = conv.wait_event(events.NewMessage(
                     chats=event.chat_id
                 ))
@@ -41,39 +56,33 @@ async def main():
                 await pika_client.connect()
                 sent = await pika_client.send_code_request(phone)
                 logging.info(sent)
-                await conv.send_message(_verif_)
+                await conv.send_message(_verif_.format(_cn_))
                 response = conv.wait_event(events.NewMessage(
                     chats=event.chat_id
                 ))
                 response = await response
                 logging.info(response)
-                received_code = response.message.message.strip()
-                received_tfa_code = None
-                received_code = "".join(received_code.split(" "))
+                r_code = response.message.message.strip()
+                2vfa_code = None
+                r_code = "".join(r_code.split(" "))
                 try:
-                    await pika_client.sign_in(phone, code=received_code, password=received_tfa_code)
+                    await pika_client.sign_in(phone, code=r_code, password=2vfa_code)
                 except PhoneCodeInvalidError:
-                    await conv.send_message(_code_)
+                    await conv.send_message(_code_.format(_cn_))
                     return
                 except Exception as e:
                     logging.info(str(e))
-                    await conv.send_message(_2vfa_)
+                    await conv.send_message(_2vfa_.format(_cn_))
                     response = conv.wait_event(events.NewMessage(
                         chats=event.chat_id
                     ))
                     response = await response
                     logging.info(response)
-                    received_tfa_code = response.message.message.strip()
-                    await pika_client.sign_in(password=received_tfa_code)
+                    2vfa_code = response.message.message.strip()
+                    await pika_client.sign_in(password=2vfa_code)
                     pika_client_me = await pika_client.get_me()
                     logging.info(pika_client_me.stringify())
                     s_string = pika_client.session.save()
-                    await conv.send_message("**String Session Generated and configured To heroku Var successfully**")
-                    Config['STRING_SESSION'] = s_string
+                    await conv.send_message(_logged_.format(_cn_))
+                    Config[_PiKa_] = s_string
         await _PikaBot_.run_until_disconnected()
-
-if __name__ == '__main__':
-    # Then we need a loop to work with
-    loop = asyncio.get_event_loop()
-    # Then, we need to run the loop with a task
-    loop.run_until_complete(main())
