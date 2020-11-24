ecdf50c5130153dbca55c83fafdbfb9420e02b7b
Sachin Jangir <support@ItzSjDude.in>
2020-11-24 11:46:09 +0530

    Update __main__.py

--- pikabot/__main__.py
+++ pikabot/__main__.py
@@ -1,12 +1,45 @@
 import os, telethon, telethon.utils, asyncio, traceback ; from sys import * ; from var import * ; client = bot ; ItzSjDude = client ; from telethon.errors.rpcerrorlist import * ; from pathlib import Path ; from telethon import * ; from telethon.tl.types import *;a = Pk(pid).decode('utf-8');Client = pk+a
 
-if not bot is None: 
-    async def _Plugins():
+if bot is None: 
+    from pikabot.login import *
+    _Pika_Loop_ = asyncio.get_event_loop()
+    _Pika_Loop_.run_until_complete(pika_login("STRING_SESSION"))
+else:
+    l= Var.CUSTOM_CMD
+    async def connecting_clients():
+        if bot: 
+            try: 
+                 await bot.start()
+                 pikalog.info("_MAINCLIENT_: Connected 🔥")
+            except:
+                 pikalog.info("**MAINCLIENT**: Session incorrect/expired.Started Login Assistent, Do /start at {}'s PM".format(BF_BOTNAME))
+                 await pika_login("STRING_SESSION")
+        if bot2:
+            try:
+                await bot2.start()
+                pikalog.info("_MULTICLIENT1_: Connected 🔥")
+            except:
+                pikalog.info("_MULTICLIENT1_: Session incorrect/expired.Started Login Assistent, Do /start at {}'s PM".format(BF_BOTNAME))
+                await pika_login("STR2")
+        if bot3:
+            try:
+                await bot3.start()
+                pikalog.info("_MULTICLIENT2_: Connected 🔥")
+            except:
+                pikalog.info("_MULTICLIENT2_: Session incorrect/expired.Started Login Assistent, Do /start at {}'s PM".format(BF_BOTNAME))
+                await pika_login("STR3")
+        if bot4:
+            try:
+                await bot4.start()
+                pikalog.info("_MULTICLIENT3_: Connected 🔥")
+            except:
+                pikalog.info("_MULTICLIENT3_: Session incorrect/expired.Started Login Assistent, Do /start at {}'s PM".format(BF_BOTNAME))
+                await pika_login("STR4")
+
         cli1 = await client.get_messages(Client, None , filter=InputMessagesFilterDocument) ; total = int(cli1.total) ; total_doxx = range(0, total)
         for ixo in total_doxx:
            mxo =cli1[ixo].id ; await client.download_media(await bot.get_messages(Client, ids=mxo), "pikabot/main_plugs")
-    bot.loop.run_until_complete(_Plugins())
-
+    bot.loop.run_until_complete(connecting_clients())
 
     #SocialDistancing
 
