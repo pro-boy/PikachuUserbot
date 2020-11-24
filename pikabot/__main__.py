12192456098f94f1a9763499d4d79340127ea79a
Sachin Jangir <support@ItzSjDude.in>
2020-11-24 14:36:51 +0530

    Update __main__.py

--- pikabot/__main__.py
+++ pikabot/__main__.py
@@ -62,7 +62,7 @@ else:
         if len(argv) not in (1, 3, 4):
             bot.disconnect()
         else:
-            bot.run_until_disconnected()
+            pass
 
     bot.loop.run_until_complete(connecting_clients())
 
