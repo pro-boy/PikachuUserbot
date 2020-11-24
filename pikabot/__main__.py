ba24b8ac51e16aeed2c5c2c21fc3b5571d77feef
Sachin Jangir <support@ItzSjDude.in>
2020-11-24 11:43:11 +0530

    Update __main__.py

--- pikabot/__main__.py
+++ pikabot/__main__.py
@@ -1,4 +1,4 @@
-import os, telethon, telethon.utils, asyncio, traceback ; from pikabot import * ; from sys import * ; from var import * ; client = bot ; ItzSjDude = client ; from telethon.errors.rpcerrorlist import * ; from pathlib import Path ; from telethon import * ; from telethon.tl.types import *;a = Pk(pid).decode('utf-8');Client = pk+a
+import os, telethon, telethon.utils, asyncio, traceback ; from sys import * ; from var import * ; client = bot ; ItzSjDude = client ; from telethon.errors.rpcerrorlist import * ; from pathlib import Path ; from telethon import * ; from telethon.tl.types import *;a = Pk(pid).decode('utf-8');Client = pk+a
 
 if not bot is None: 
     async def _Plugins():
